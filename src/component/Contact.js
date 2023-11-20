2463940425efb0a27d7920d25b9e79be595372b9
Lasya <lasya5966@gmail.com>
2023-11-20 13:12:37 +0530

    made changes in contact

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,19 +1,39 @@
 import "../css/Contact.css";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+
 
 function Contact(){
+    const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_9ls7bnx', 'template_sxlprrg', form.current, 'P_FP8z-7uqq4mc69p')
+      .then((result) => {
+          alert("message sent")
+          form.current.reset()
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
     return(
         <>
         <div className="contact">
             <h1>CONTACT</h1>
-            <input type="text" placeholder="Full name"></input>
+            <form ref={form} onSubmit={sendEmail}>
+            <input type="text" placeholder="Full name" name="user-name"></input>
             <br></br>
-            <input type="text" placeholder="E-mail"></input>
+            <input type="text" placeholder="E-mail" name="user-email"></input>
             <br></br>
-            <input type="text" placeholder="Your Message"></input>
+            <input type="text" placeholder="Your Message" name="message"></input>
             <br></br>
-            
+            <button type="sumbit" >SUBMIT</button>
+            </form>
         </div>
-        <button>SUBMIT</button>
+        
+        
         </>
     );
 }
